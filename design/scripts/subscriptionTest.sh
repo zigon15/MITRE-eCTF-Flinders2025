44643babb468c84d7c6e09607c80033fe9993613
Simon <ss13andbb12@gmail.com>
2025-02-10 00:43:28 +1300

    misc: Minor fixes

--- design/scripts/subscriptionTest.sh
+++ design/scripts/subscriptionTest.sh
@@ -1,2 +1,3 @@
-python3 ../ectf25_design/gen_subscription.py ../output/secrets.bin ../output/subscription.bin 0xDEADBEEF 32 128 1 --force
+mkdir ../output
+python3 ../ectf25_design/gen_subscription.py ../../global.secrets/secrets.bin ../output/subscription.bin 0xDEADBEEF 32 128 1 --force
 # python3 ../ectf25_design/gen_subscription.py ../output/secrets.bin ../output/subscription.bin 0x12982789 32 128 3 --force
