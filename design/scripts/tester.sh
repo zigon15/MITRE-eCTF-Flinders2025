23b3a6cf006aa2aa3669a0bfdc360eb1c7335b88
Simon <ss13andbb12@gmail.com>
2025-02-20 19:39:04 +1030

    design: Fixes

--- design/scripts/tester.sh
+++ design/scripts/tester.sh
@@ -1,15 +1,15 @@
 mkdir output
 
-python3 -m ectf25_design.gen_subscription ../../global.secrets/secrets.bin ./output/subscription_c1.bin 0xDEADBEEF 32 128 1 --force
+python3 -m ectf25_design.gen_subscription ../../global.secrets/secrets.bin ./output/subscription_c1.bin 0xDEADBEEF 0 18446744073709551615 1 --force
 python -m ectf25.tv.subscribe ./output/subscription_c1.bin /dev/ttyACM0
 
-python3 -m ectf25_design.gen_subscription ../../global.secrets/secrets.bin ./output/subscription_c2.bin 0xDEADBEEF 32 128 2 --force
+python3 -m ectf25_design.gen_subscription ../../global.secrets/secrets.bin ./output/subscription_c2.bin 0xDEADBEEF 0 18446744073709551615 2 --force
 python -m ectf25.tv.subscribe ./output/subscription_c2.bin /dev/ttyACM0
 
-python3 -m ectf25_design.gen_subscription ../../global.secrets/secrets.bin ./output/subscription_c3.bin 0xDEADBEEF 32 128 3 --force
+python3 -m ectf25_design.gen_subscription ../../global.secrets/secrets.bin ./output/subscription_c3.bin 0xDEADBEEF 0 18446744073709551615 3 --force
 python -m ectf25.tv.subscribe ./output/subscription_c3.bin /dev/ttyACM0
 
-python3 -m ectf25_design.gen_subscription ../../global.secrets/secrets.bin ./output/subscription_c4.bin 0xDEADBEEF 32 128 4 --force
+python3 -m ectf25_design.gen_subscription ../../global.secrets/secrets.bin ./output/subscription_c4.bin 0xDEADBEEF 0 18446744073709551615 4 --force
 python -m ectf25.tv.subscribe ./output/subscription_c4.bin /dev/ttyACM0
 
 python -m ectf25.utils.tester -s="../../global.secrets/secrets.bin" -p="/dev/ttyACM0" --perf rand 
