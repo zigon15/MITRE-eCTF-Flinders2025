61dade9f4107bce1e519dc09b471603fd77a5aa1
Simon <ss13andbb12@gmail.com>
2025-02-28 08:19:21 +1030

    decoder: Docker build fix

--- design/scripts/genGlobalSecrets.sh
+++ design/scripts/genGlobalSecrets.sh
@@ -1,2 +1,1 @@
-mkdir ../../global.secrets
-python3 ../ectf25_design/gen_secrets.py ../../global.secrets/secrets.bin 1 2 3 4 --force
+python3 ../ectf25_design/gen_secrets.py ../../global.secrets 1 2 3 4 --force
