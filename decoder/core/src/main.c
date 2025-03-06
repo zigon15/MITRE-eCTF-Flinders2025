e94a36544c01634fa147fc1e4161b06874bea0dd
kissellco <kissellco@users.noreply.github.com>
2025-03-06 15:07:19 +1100

    Amended main.c

--- decoder/core/src/main.c
+++ decoder/core/src/main.c
@@ -7,25 +7,36 @@
  * @copyright Copyright (c) 2025 The MITRE Corporation
  */
 
+// C inclusions
 #include <stdio.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
+// FreeRTOS inclusions
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 #include "portmacro.h"
 #include "task.h"
 #include "semphr.h"
 
+// MAX78000 inclusions
+#include <max78000.h>
+#include "board.h"
 #include "mxc_device.h"
-#include "wut.h"
 #include "uart.h"
 #include "lp.h"
 #include "led.h"
-#include "icc.h"
 #include "trng.h"
-#include "board.h"
+#include "tmr.h"
+#include "wut.h"
+#include "icc.h"
 #include "mxc_delay.h"
+
+// MITRE inclusions
+#include "status_led.h"
+#include "simple_uart.h"
+#include "simple_flash.h"
 
 // Include tasks
 #include "crypto_manager.h"
@@ -34,57 +45,36 @@
 #include "channel_manager.h"
 #include "frame_manager.h"
 
-#include "status_led.h"
+/* =================================================================
+Configuration items 
+================================================================= */
+/* Explicitly disable tickless mode */
+unsigned int disable_tickless = 1;
 
-// Task IDs
+/* Stringification macros */
+#define STRING(x) STRING_(x)
+#define STRING_(x) #x
+
+/* =================================================================
+Task Setup 
+================================================================= */
+/* Task IDs */
 TaskHandle_t crypto_manager_task_id;
 TaskHandle_t subscription_manager_task_id;
 TaskHandle_t serial_interface_manager_task_id;
 TaskHandle_t channel_manager_task_id;
 TaskHandle_t frame_manager_task_id;
 
-/** @brief Called if stack smashing is detected
- *
-*/
-__attribute__((noreturn)) void __wrap___stack_chk_fail(void) {
-    STATUS_LED_RED();
-    printf("Stack Smashing Detected :(\n");
-    printf("Reseting :(\n");
-
-    // Wait for serial to flush then reset
-    MXC_Delay(1000000);
-    NVIC_SystemReset();
-    while(1);
-}
-
-/** @brief Called by default fortify failure handler if buffer overflow is detected
- *
-*/
-__attribute__((noreturn)) void __wrap___chk_fail(void) {
-    STATUS_LED_RED();
-    printf("Buffer Overflow (I think?!?!) [https://github.dev/lattera/glibc/blob/master/debug/chk_fail.c]\n");
-    printf("Reseting :(\n");
-
-    // Wait for serial to flush then reset
-    MXC_Delay(1000000);
-    NVIC_SystemReset();
-    while(1);
-}
-
+/* =| main |==============================================
+ * =====================================================*/
 int main(void){
-    // Delay to prevent bricks
-    volatile int i;
-    for (i = 0; i < 0xFFFFF; i++) {}
 
-    // Enable instruction cache for better performance
-    MXC_ICC_Enable(MXC_ICC0);
+    Board_Init();
+    LED_Init();
 
-    MXC_TRNG_Init();
-    
-    // Print banner (RTOS scheduler not running)
-    // printf("\n-=- %s FreeRTOS (%s) Demo -=-\n", STRING(TARGET), tskKERNEL_VERSION_NUMBER);
-    // printf("SystemCoreClock = %d\n", SystemCoreClock);
+    printf("Creating tasks...\n");
 
+    /* eCTF Tasks */
     //-- Configure Tasks --// 
     int ret;
     
@@ -148,15 +138,13 @@ int main(void){
         while(1);
     }
 
-    // Start scheduler
-    // printf("Starting scheduler.\n");
+    /* Start scheduler */
+    printf("Starting scheduler...\n\n");
     vTaskStartScheduler();
 
-    // This code is only reached if the scheduler failed to start
-    printf("ERROR: FreeRTOS did not start due to above error!\n");
-    while (1) {
-        __NOP();
-    }
-
+    /* This code is only reached if the scheduler failed to start */
+    printf("FreeRTOS scheduler failed to start! Activating failsafe...\n");
+    failsafe();
+    
     return -1;
 }
